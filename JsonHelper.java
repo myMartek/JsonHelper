37fa5b6ca24d90592f7fb21533d4fad39d864a7f
myMartek <info@mymartek.de>
2016-02-22 14:19:22 +0100

    Added iterable interfaces

--- JsonHelper.java
+++ JsonHelper.java
@@ -4,11 +4,107 @@ import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
+import java.util.Iterator;
+import java.util.Map;
+
 /**
  * Created by Marcel Wlotzka on 22.02.16.
  */
-public class JsonHelper {
+public class JsonHelper implements Iterable<JsonHelper> {
     private JsonElement element;
+
+    public final class JsonHelperIterator implements Iterator<JsonHelper> {
+        private int index = 0;
+
+        public boolean hasNext() {
+            return index < size();
+        }
+
+        public JsonHelper next() {
+            return get(index++);
+        }
+
+        public void remove() {
+            throw new UnsupportedOperationException("This class is read-only");
+        }
+    }
+
+    public final class JsonHelperEntry<String, JsonHelper> implements Map.Entry<String, JsonHelper> {
+        private final String key;
+        private JsonHelper value;
+
+        public JsonHelperEntry(String key, JsonHelper value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        @Override
+        public String getKey() {
+            return key;
+        }
+
+        @Override
+        public JsonHelper getValue() {
+            return value;
+        }
+
+        @Override
+        public JsonHelper setValue(JsonHelper object) {
+            JsonHelper old = this.value;
+
+            this.value = object;
+            return old;
+        }
+    }
+
+    public final class JsonHelperObjectIterator implements Iterable<JsonHelperEntry<String, JsonHelper>> {
+        public JsonHelperObjectIterator() {
+            super();
+        }
+        @Override
+        public Iterator<JsonHelperEntry<String, JsonHelper>> iterator() {
+            return new JsonHelperActualObjectIterator();
+        }
+
+        public final class JsonHelperActualObjectIterator implements Iterator<JsonHelperEntry<String, JsonHelper>> {
+            private Iterator<Map.Entry<String, JsonElement>> iterator;
+
+            public JsonHelperActualObjectIterator() {
+                super();
+
+                if (element != null && element.isJsonObject()) {
+                    iterator = element.getAsJsonObject().entrySet().iterator();
+                }
+            }
+
+            @Override
+            public boolean hasNext() {
+                if (iterator != null) {
+                    return iterator.hasNext();
+                }
+
+                return false;
+            }
+
+            @Override
+            public JsonHelperEntry<String, JsonHelper> next() {
+                if (iterator != null) {
+                    Map.Entry<String, JsonElement> el = iterator.next();
+
+                    return new JsonHelperEntry<>(el.getKey(), new JsonHelper(el.getValue()));
+                }
+
+                return null;
+            }
+
+            @Override
+            public void remove() {
+                if (iterator != null) {
+                    iterator.remove();
+                }
+            }
+        }
+    }
 
     public JsonHelper() {
         this.element = null;
@@ -50,7 +146,7 @@ public class JsonHelper {
         return 0;
     }
 
-    public JsonHelper at(int index) {
+    public JsonHelper get(int index) {
         if (element == null) {
             return this;
         }
@@ -169,5 +265,14 @@ public class JsonHelper {
 
         Gson gson = new Gson();
         return gson.toJson(element);
+    }
+
+    public JsonHelperObjectIterator objectIterator() {
+        return new JsonHelperObjectIterator();
+    }
+
+    @Override
+    public Iterator<JsonHelper> iterator() {
+        return new JsonHelperIterator();
     }
 }
